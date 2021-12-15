58830e4aebd6c0f5c919287a6b392e5576182797
silkehof <silke@silkehofmann.eu>
2021-12-15 21:20:35 +0100

    Add all project files

---
+++ reverser.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    string text;
+    string line;
+
+    ifstream file("test.txt");
+    while (getline (file, line)) {
+        text += line;
+    }
+
+    string result;
+    for(int i = text.length() - 1; i >= 0; i--) {
+        result += text.at(i);
+    }
+
+    cout << text + "\n";
+    cout << result + "\n";
+
+    file.close();
+}
